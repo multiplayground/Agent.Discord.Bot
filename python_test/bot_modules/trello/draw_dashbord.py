3bc2dbc1275ad687b5a9b52df704dbcd14e95c25
SpIIIII <Shipin16@gmail.com>
2019-09-22 20:16:50 +0300

    v0.0.5 add label of the card to DataFrame in get_trello_scare

--- python_test/bot_modules/trello/draw_dashbord.py
+++ python_test/bot_modules/trello/draw_dashbord.py
@@ -6,7 +6,7 @@ import numpy as np
 import json
 import os
 
-from .get_trelo_score import get_trello_data
+from get_trelo_score import get_trello_data
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -18,20 +18,21 @@ def make_dataframe_by(value:str=None):
     first 3 columns contain that required data such:    '0' name of card
                                                         '1' data when card have been started
                                                         '2' data when card finished if so
+                                                        '3' label of the card
     all other columns contain data by with we can choose them:
-                                                        3-(-1) member that assigned to card
+                                                        4-(-1) member that assigned to card
                                                         -1 name of list to which card belon
     you can pass string with interesting value of feature as an argument and gen whole spectre of rows
     where that feature is present: for expample you can pass neme list of cards 'Backlog'                                                    
                                                                                                             
-    '''
-    trello_data = get_trello_data()
+                        '''
+                        # trello_data = get_trello_data()
 
-                                    # with open ('trello_data.txt','w') as file:
-                                    #     file.write(json.dumps(trello_data))
+                        # with open ('trello_data.txt','w') as file:
+                        #     file.write(json.dumps(trello_data))
 
-                                    # with open('trello_data.txt','r') as file:
-                                    #     trello_data = json.load(file)
+    with open('trello_data.txt','r') as file:
+        trello_data = json.load(file)
 
     # Find maximum members per card for set index dataframe dimension
     max_len = max([len(i[2]['who']) if i[2]['who'] else 0 for i in trello_data])    
@@ -54,8 +55,10 @@ def make_dataframe_by(value:str=None):
     indeces2.iloc[:,-1]= indeces1
 
     # Collect interesting data: 1) card name 2) card start time 3) card finish time if it is
-    data = [  (el[1],t(el[2]['time_start']),t(el[2]['time_close'])) 
+    data = [  (el[1],t(el[2]['time_start']),t(el[2]['time_close']),el[2]['label']) 
                 for el in trello_data]
+
+    
     
     # Accemble result dataframe
     indeces3 = pd.DataFrame(data) 
@@ -92,7 +95,7 @@ def draw_dashbord():
             forth_ax = fig.add_subplot(grid[-1,-1],projection = '3d')
         
         # Pass needed data to first axes
-        hist_ax.hist(data[5],label = 'Колличество задач')
+        hist_ax.hist(data[6],label = 'Колличество задач')
         hist_ax.legend(loc = 'upper right')
         
         # Pass needed data to second axes
@@ -103,12 +106,10 @@ def draw_dashbord():
         main_ax.locator_params(axis = 'y',nbins = 10)
 
         # Pass needed data to therd axes
-        third_ax
-
         
         fig.subplots_adjust(left=0.02, right=0.99, bottom=0.03, top=0.99)
-        plt.savefig(static+'/ceres_dashbord.png')
-        # plt.show()
+        # plt.savefig(static+'/ceres_dashbord.png')
+        plt.show()
 
 def t(time:str=None):
     '''
@@ -126,9 +127,14 @@ def select_from_dataframe(data:pd.DataFrame=None,value:str=None):
         return data[(data==value).any(axis=1)]
     return data
 
+def sub_frame(data:pd.DataFrame,label:str):
+    data_by = select_from_dataframe(data,'backend')
+    result = pd.DataFrame(index = data_by.sort_values(1)[1])
+    result
+    return result
+
 
 
 if __name__ == '__main__':
-    # print(make_dataframe_by())
-    print(make_dataframe_by())
+    print(sub_frame(make_dataframe_by(),'frontend'))
     # draw_dashbord()
